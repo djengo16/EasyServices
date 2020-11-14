d34ad3345867a2a9246a77d785b4d55096d2da76
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-11-14 19:28:13 +0200

    add viewmodels and some constants

--- EasyServices.Common/GlobalConstants.cs
+++ EasyServices.Common/GlobalConstants.cs
@@ -5,5 +5,19 @@
         public const string SystemName = "EasyServices";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string DefaultAnnouncementImg = "https://www.pacificfoodmachinery.com.au/media/catalog/product/placeholder/default/no-product-image-400x400.png";
+
+        public const string AnnouncementStarImg = "https://www.flaticon.com/svg/static/icons/svg/616/616655.svg";
+
+        public const string LocationIcon = "https://i.ibb.co/80qsyFp/placeholder-1.png";
+
+        public const string MailIcon = "https://i.ibb.co/Ch0FNwp/mail.png";
+
+        public const string PhoneIcon = "https://i.ibb.co/tP2ZCqw/telephone.png";
+
+        public const string WebSiteIcon = "https://i.ibb.co/K9n3vv3/globe.png";
+
+        public const string DefaultProfilePicture = "https://icon-library.com/images/default-profile-icon/default-profile-icon-16.jpg";
     }
 }
--- Services/EasyServices.Services.Data/CitiesService.cs
+++ Services/EasyServices.Services.Data/CitiesService.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EasyServices.Services.Data
+﻿namespace EasyServices.Services.Data
 {
-    class CitiesService
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using EasyServices.Services.Data;
+    using EasyServices.Data.Common.Repositories;
+    using EasyServices.Data.Models;
+
+    public class CitiesService : ICitiesService
     {
+        private readonly IDeletableEntityRepository<City> citiesRepository;
+
+        public CitiesService(IDeletableEntityRepository<City> citiesRepository)
+        {
+            this.citiesRepository = citiesRepository;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> GetAllAsKeyValuePairs()
+        {
+            return this.citiesRepository.AllAsNoTracking()
+                .Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                }).ToList().Select(x => new KeyValuePair<string, string>(x.Id.ToString(), x.Name));
+        }
+
+        public City GetCityById(int cityId)
+        {
+            return this.citiesRepository.All()
+                .FirstOrDefault(x => x.Id == cityId);
+        }
     }
 }
