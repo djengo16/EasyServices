29927775d74a3782782f44f0b9559748c022f5cd
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-11-21 19:16:00 +0200

    modify some identity pages

---
+++ Web/EasyServices.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,139 @@
+﻿namespace EasyServices.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+
+    using CloudinaryDotNet;
+    using EasyServices.Data.Models;
+    using EasyServices.Services;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly Cloudinary cloudinary;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            Cloudinary cloudinary)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            this.cloudinary = cloudinary;
+        }
+
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Display(Name = "Имена")]
+            public string Name { get; set; }
+
+            [Phone]
+            [Display(Name = "Телефон")]
+            public string PhoneNumber { get; set; }
+
+            [Display(Name = "Описание")]
+            public string DescriptionInfo { get; set; }
+
+            [Display(Name = "Уебсайт")]
+            public string WebSite { get; set; }
+
+            [Display(Name = "Профилна снимка")]
+
+            public string ProfilePictureUrl { get; set; }
+
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Input = new InputModel
+            {
+                Name = user.Name,
+                PhoneNumber = phoneNumber,
+                DescriptionInfo = user.Description,
+                WebSite = user.WebSite,
+                ProfilePictureUrl = user.ProfilePicture,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile file)
+        {
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    StatusMessage = "Unexpected error when trying to set phone number.";
+                    return RedirectToPage();
+                }
+            }
+
+            if (this.Input.Name != user.Name)
+            {
+                user.Name = this.Input.Name;
+            }
+
+            if (this.Input.DescriptionInfo != user.Description)
+            {
+                user.Description = this.Input.DescriptionInfo;
+            }
+
+            if (this.Input.WebSite != user.WebSite)
+            {
+                user.WebSite = this.Input.WebSite;
+            }
+
+            if (file != null)
+            {
+                await CloudinaryHelper.RemoveFileAsync(this.cloudinary, user.ProfilePicture); // Deleting the old img
+
+                var imageUrl = await CloudinaryHelper.UploadFileAsync(this.cloudinary, file, true);
+                user.ProfilePicture = imageUrl;
+            }
+
+            await _userManager.UpdateAsync(user);
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Профила е обновен";
+            return RedirectToPage();
+        }
+    }
+}
---
+++ Web/EasyServices.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,123 @@
+﻿namespace AspNetCoreTemplate.Web.Areas.Identity.Pages.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+    using EasyServices.Common;
+    using EasyServices.Data.Models;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+    using Microsoft.Extensions.Logging;
+
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly IEmailSender _emailSender;
+
+        public RegisterModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<RegisterModel> logger,
+            IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress(ErrorMessage = "Имейлът не е валиден!")]
+            [Display(Name = "Имейл")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(50, ErrorMessage = "Паролата трябва да бъде най-малко {2} и най-много {1} символа.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Парола")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Потвърдете паролата")]
+            [Compare("Password", ErrorMessage = "Паролите не съвпадат.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = this.Input.Email,
+                    Email = this.Input.Email,
+                    ProfilePicture = GlobalConstants.DefaultProfilePicture,
+                };
+
+                var result = await this._userManager.CreateAsync(user, this.Input.Password);
+
+                if (result.Succeeded)
+                {
+                    this._logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
