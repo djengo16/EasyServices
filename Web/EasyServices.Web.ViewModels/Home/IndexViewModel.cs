c3238dcc839506aa035c79448540ce45a3f69f69
djengo16 <53972425+djengo16@users.noreply.github.com>
2020-11-24 18:34:02 +0200

    modify home,edit page add search function

---
+++ Web/EasyServices.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyServices.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    using EasyServices.Web.ViewModels.Announcements;
+
+    public class IndexViewModel
+    {
+        public IEnumerable<AnnouncementViewModel> Announcements { get; set; }
+
+        public IEnumerable<AnnouncementCategoryInputModel> Categories { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> CitiesItems { get; set; }
+    }
+}
--- Web/EasyServices.Web/Controllers/HomeController.cs
+++ Web/EasyServices.Web/Controllers/HomeController.cs
@@ -2,15 +2,36 @@
 {
     using System.Diagnostics;
 
+    using EasyServices.Services.Data;
     using EasyServices.Web.ViewModels;
+    using EasyServices.Web.ViewModels.Home;
+    using EasyServices.Web.ViewModels.Announcements;
 
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
+        private readonly IAnnouncementsService announcementsService;
+        private readonly ICitiesService citiesService;
+        private readonly ICategoriesService categoriesService;
+
+        public HomeController(IAnnouncementsService announcementsService, ICitiesService citiesService, ICategoriesService categoriesService)
+        {
+            this.announcementsService = announcementsService;
+            this.citiesService = citiesService;
+            this.categoriesService = categoriesService;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            IndexViewModel viewModel = new IndexViewModel
+            {
+                Announcements = this.announcementsService.GetLast<AnnouncementViewModel>(8),
+                CitiesItems = this.citiesService.GetAllAsKeyValuePairs(),
+                Categories = this.categoriesService.GetCategoriesAndSubCategories<AnnouncementCategoryInputModel>(),
+            };
+
+            return this.View(viewModel);
         }
 
         public IActionResult Privacy()
